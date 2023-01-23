c59fe1483a5edfef056ebe46b7a758f7ef377a20
Pavi <pavanirannulu@gmail.com>
2023-01-23 10:27:51 -0800

    Initial Commit

---
+++ HomeBuddies/HomeBuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeBuddiesApp.swift
+//  HomeBuddies
+//
+//  Created by Pavi Rannulu on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeBuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
